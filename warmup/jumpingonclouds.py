920132b3c0a322cc98cd18a26070d6dbef7360b6
awim <muhammadtaqwiem@gmail.com>
2019-08-07 11:52:19 +0700

    solvin jumping on the clouds

---
+++ warmup/jumpingonclouds.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+# Complete the jumpingOnClouds function below.
+def jumpingOnClouds(c):
+    m = 0
+    i = 0
+    l = len(c)
+    
+    while i < l:
+        if i+2 < l and c[i+2] == 0:
+            i = i+2
+        else:
+            i = i+1
+
+        if i >= l:
+            break 
+        m += 1
+    return m
+
+
+
+""" if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    n = int(input())
+    c = list(map(int, input().rstrip().split()))
+    result = jumpingOnClouds(c)
+    fptr.write(str(result) + '\n')
+    fptr.close() """
+
+c = [0,1,0,0,0,1,0,1,0,0,1,0,0,0,0,0,1,0,1,0,0,1]
+print(jumpingOnClouds(c))
