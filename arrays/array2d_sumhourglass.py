1480959da809b89bc2b517461ae2826e5ba1cd48
awim <muhammadtaqwiem@gmail.com>
2019-08-09 08:49:43 +0700

    save max if greater or equal _max count

--- arrays/array2d_sumhourglass.py
+++ arrays/array2d_sumhourglass.py
@@ -32,15 +32,15 @@ def hourglassSum(arr):
         _mid = midInRowSplit(3, arr[i+1])
         _bot = sumInRowSplit(3, arr[i+2])
 
-        # print(_top)
-        # print(_mid)
-        # print(_bot)
-        # print('---------')
+        print(_top)
+        print(_mid)
+        print(_bot)
+        print('---------')
 
         
         for j,b in enumerate(_top):
             max = b + _mid[j] + _bot[j]
-            if max > _max:
+            if max >= _max:
                 _max = max
 
     return _max
@@ -69,6 +69,24 @@ arr = [
     [0, 0, 2, 4, 4, 0],
     [0, 0, 0, 2, 0, 0],
     [0, 0, 1, 2, 4, 0]
+]
+
+arr = [
+    [-1, -1, 0, -9, -2, -2],
+    [-2, -1, -6, -8, -2, -5],
+    [-1, -1, -1, -2, -3, -4],
+    [-1, -9, -2, -4, -4, -5],
+    [-7, -3, -3, -2, -9, -9],
+    [-1, -3, -1, -2, -4, -5]
+]
+
+arr = [
+    [-1, 1, -1, 0, 0, 0],
+    [0, -1, 0, 0, 0, 0],
+    [-1, -1, -1, 0, 0, 0],
+    [0, -9, 2, -4, -4, 0],
+    [-7, 0, 0, -2, 0, 0],
+    [0, 0, -1, -2, -4, 0]
 ]
 
 print (hourglassSum(arr))
